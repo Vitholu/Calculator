9fc17f31c1bb778c5cc43a93f4068098648bc2a3
Vitholu <vitholumtech@gmail.com>
2022-11-01 14:57:43 +0100

    Add basic functionality to the calculator

--- script.js
+++ script.js
@@ -1,60 +1,151 @@
-// Display Result
 const btnClick = document.querySelectorAll('button');
-const displayNumbers = document.querySelector('.result');
-let arrayNumbers = [];
+let displayNumbers = document.querySelector('.result');
+
+let answer;
+let reset;
+let blockInOne;
+let inOne;
+let inTwo;
+let lastOperator = [];
 
 btnClick.forEach((e) => {
 	e.addEventListener('click', (e) => {
-		console.log(e.target.id);
-		if (e.target.id >= 0 || e.target.id === '.') {
-			if (arrayNumbers.indexOf(0) === 0) {
-				arrayNumbers.pop();
-			}
-			arrayNumbers.push(e.target.id);
-			for (const n of arrayNumbers) {
-				displayNumbers.textContent = arrayNumbers.join('');
-			}
-		}
-		console.log(arrayNumbers);
-
-		if (e.target.id === 'cls') {
-			return clearAll(true);
+		switch (e.target.id) {
+			case 'cls':
+				displayNumbers.innerText = '';
+				answer = '';
+				reset = '';
+				blockInOne = '';
+				inOne = '';
+				inTwo = '';
+				lastOperator = [];
+				break;
+			case 'bsp':
+				displayNumbers.innerText = displayNumbers.innerText.slice(0, -1);
+				break;
+			case '+':
+				displayNumbers.innerText += e.target.id;
+				if (displayNumbers.innerText.includes('+') && blockInOne !== 1) {
+					inOne = displayNumbers.innerText.slice(0, -1);
+					displayNumbers.innerText = '';
+					blockInOne = 1;
+					lastOperator.unshift('+');
+					console.log(inOne);
+				}
+				if (displayNumbers.innerText.includes('+')) {
+					inTwo = displayNumbers.innerText.slice(0, -1);
+					answer = +inOne + +inTwo;
+					displayNumbers.innerText = answer;
+					blockInOne = 0;
+					reset = 1;
+				}
+				break;
+			case '-':
+				displayNumbers.innerText += e.target.id;
+				if (displayNumbers.innerText.includes('-') && blockInOne !== 1) {
+					inOne = displayNumbers.innerText.slice(0, -1);
+					displayNumbers.innerText = '';
+					blockInOne = 1;
+					lastOperator.unshift('-');
+					console.log(inOne);
+				}
+				if (displayNumbers.innerText.includes('-')) {
+					inTwo = displayNumbers.innerText.slice(0, -1);
+					answer = inOne - inTwo;
+					displayNumbers.innerText = answer;
+					blockInOne = 0;
+					reset = 1;
+				}
+				break;
+			case '/':
+				displayNumbers.innerText += e.target.id;
+				if (displayNumbers.innerText.includes('/') && blockInOne !== 1) {
+					inOne = displayNumbers.innerText.slice(0, -1);
+					displayNumbers.innerText = '';
+					blockInOne = 1;
+					lastOperator.unshift('/');
+					console.log(inOne);
+				}
+				if (displayNumbers.innerText.includes('/')) {
+					inTwo = displayNumbers.innerText.slice(0, -1);
+					answer = inOne / inTwo;
+					displayNumbers.innerText = answer;
+					blockInOne = 0;
+					reset = 1;
+				}
+				break;
+			case '*':
+				displayNumbers.innerText += e.target.id;
+				if (displayNumbers.innerText.includes('*') && blockInOne !== 1) {
+					inOne = displayNumbers.innerText.slice(0, -1);
+					displayNumbers.innerText = '';
+					blockInOne = 1;
+					lastOperator.unshift('*');
+					console.log(inOne);
+				}
+				if (displayNumbers.innerText.includes('*')) {
+					inTwo = displayNumbers.innerText.slice(0, -1);
+					answer = inOne * inTwo;
+					displayNumbers.innerText = answer;
+					blockInOne = 0;
+					reset = 1;
+				}
+				break;
+			case '%':
+				displayNumbers.innerText += e.target.id;
+				if (displayNumbers.innerText.includes('%') && blockInOne !== 1) {
+					inOne = displayNumbers.innerText.slice(0, -1);
+					displayNumbers.innerText = '';
+					blockInOne = 1;
+					lastOperator.unshift('%');
+					console.log(inOne);
+				}
+				if (displayNumbers.innerText.includes('%')) {
+					inTwo = displayNumbers.innerText.slice(0, -1);
+					answer = inOne % inTwo;
+					displayNumbers.innerText = answer;
+					blockInOne = 0;
+					reset = 1;
+				}
+				break;
+			case '=':
+				if (inOne !== '' && displayNumbers.innerText !== '') {
+					let operator = lastOperator[0];
+					switch (operator) {
+						case '%':
+							answer = +inOne % +displayNumbers.innerText;
+							displayNumbers.innerText = answer;
+							break;
+						case '/':
+							answer = +inOne / +displayNumbers.innerText;
+							displayNumbers.innerText = answer;
+							break;
+						case '*':
+							answer = +inOne * +displayNumbers.innerText;
+							displayNumbers.innerText = answer;
+							break;
+						case '-':
+							answer = +inOne - +displayNumbers.innerText;
+							displayNumbers.innerText = answer;
+							break;
+						case '+':
+							answer = +inOne + +displayNumbers.innerText;
+							displayNumbers.innerText = answer;
+							break;
+						default:
+							break;
+					}
+				}
+				break;
+			default:
+				if (reset) {
+					inOne = answer;
+					answer = '';
+					displayNumbers.innerText = e.target.id;
+					reset = 0;
+				} else {
+					displayNumbers.innerText += e.target.id;
+				}
 		}
 	});
 });
-
-// Operators
-
-function sum(a, b) {
-	return sum;
-}
-
-function subtract(a, b) {
-	return subtract;
-}
-
-function multiply(a, b) {
-	return multiply;
-}
-
-function divide(a, b) {
-	return divide;
-}
-
-// Misc
-
-function clearAll(x) {
-	if (x) {
-		arrayNumbers = [0];
-		console.log('made it to clearAll');
-		displayNumbers.textContent = arrayNumbers.join('');
-	}
-}
-
-function remove(x) {
-	return remove;
-}
-
-function equal(x) {
-	return equal;
-}
